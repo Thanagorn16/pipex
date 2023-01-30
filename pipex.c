c8382a408fa3ae55163abdb834c75108c54d3392
Thanagorn Ruangsit <thanagorn1611@gmail.com>
2023-01-30 10:42:16 +0700

    add path

--- pipex.c
+++ pipex.c
@@ -1,15 +1,11 @@
 #include "pipex.h"
 
-int main(int ac, char **av, char **envp)
+char    **find_path(char **envp)
 {
     int i;
     char    *path;
     char    **path_env;
-    // char    *path = "/bin/ls";
-    char    *args[] = {"ls", "-l", NULL};
 
-    // execve(path, args, NULL);
-    // printf(envp[i]);
     i = 0;
     while (envp[i]) // get the path index
     {
@@ -22,10 +18,40 @@ int main(int ac, char **av, char **envp)
     path_env = ft_split(path, ':'); // split them with :
     i = 0;
     path_env[i] = ft_strtrim(path_env[i], "PATH="); // trim PATH=
-    while (path_env[i])
+    return (path_env);
+}
+
+int main(int ac, char **av, char **envp)
+{
+    char    **path_env;
+    char    *test_path = "/bin/ls"; //* this is the correct path for "ls". The others are fault.
+    // char    *test_path2 = "/bin/ls"; 
+    char    *temp_path;
+    char    **cmd;
+    char    *args[] = {"ls", "-l", NULL};
+    int fd[2];
+    int pid;
+
+    path_env = find_path(envp);
+    if (pipe == -1)
+        return (1);
+    pid = fork;
+    if (pid < 0)
+        return (2);
+    if (pid == 0)
     {
-        printf("%s\n", path_env[i++]);
+        // child process
+        
     }
-    // printf("%s\n", path);
-    // printf("len:%zu\n", ft_strlen(path));
+    // *test
+    // cmd = ft_split(av[1], ' '); // split command and argument to the commnad from each other
+    // temp_path = ft_strjoin_path(path_env[i], cmd[0]);
+    // printf("%s\n", temp_path);
+    // printf("%s\n", path_env[0]);
+    // execve(test_path, cmd, NULL);
+    // while (path_env[i])
+    // {
+    //     printf("%s\n", path_env[i++]);
+    // }
+    
 }
--- pipex.h
+++ pipex.h
@@ -9,6 +9,7 @@ size_t	ft_strlen(const char *str);
 char	**ft_split(char const *s, char c);
 char	*ft_strdup(const char *str);
 char	*ft_strtrim(char const *str, char const *set);
+char	*ft_strjoin_path(char const *str1, char const *str2);
 
 
 #endif
